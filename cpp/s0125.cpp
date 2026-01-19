d050e3319e9c4d877276048334b1f79f8fa9d0dc
Ruslan Karabalin <karabalin112@gmail.com>
2026-01-19 11:17:15 +0300

    feat: add s0125

---
+++ cpp/s0125.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+bool checkChar(char c) {
+    return ('A' <= c && c <= 'Z') || ('a' <= c && c <= 'z') || ('0' <= c && c <= '9');
+}
+
+public:
+    bool isPalindrome(string s) {
+        int n = s.length();
+        if (n == 0) {
+            return false;
+        }
+        if (n == 1) {
+            return true;
+        }
+
+        vector<char> chars;
+        for (char c : s) {
+            if (checkChar(c)) {
+                if (c <= 'Z') {
+                    c += 'a' - 'Z' + 25;
+                }
+                chars.push_back(c);
+            }
+        }
+
+        // for (char c : chars) {
+        //     cout << c;
+        // }
+        // cout << "\n";
+
+        int l = 0;
+        int r = chars.size() - 1;
+        while (l < r) {
+            char lc = chars[l];
+            char rc = chars[r];
+            // cout << "lc: " << lc << ", rc: " << rc << "\n";
+            if (lc != rc) {
+                return false;
+            }
+            ++l;
+            --r;
+        }
+        return true;
+    }
+};
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    Solution s;
+    cout << s.isPalindrome("Zeus was deified, saw Suez.") << "\n";
+
+    return 0;
+}
