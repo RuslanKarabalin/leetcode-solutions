c88b386fe573876e8101970eec3fdcde34da574e
Ruslan Karabalin <karabalin112@gmail.com>
2026-01-21 09:29:36 +0300

    feat: add s0704

---
+++ cpp/s0704.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+   public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int l = 0;
+        int h = n - 1;
+        while (l <= h) {
+            int m = l + (h - l) / 2;
+            if (target < nums[m]) {
+                h = m - 1;
+            } else if (target > nums[m]) {
+                l = m + 1;
+            } else {
+                return m;
+            }
+        }
+        return -1;
+    }
+};
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    vector<int> in0 = {-1, 0, 3, 5, 9, 12};
+    int t0 = 9;
+
+    vector<int> in1 = {-1, 0, 3, 5, 9, 12};
+    int t1 = 2;
+
+    Solution s;
+
+    cout << s.search(in0, t0) << "\n";
+    cout << s.search(in1, t1) << "\n";
+
+    return 0;
+}
