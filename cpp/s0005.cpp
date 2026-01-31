d80d88e3b7cf545c6c888f36a1109eba4c2f3e26
Ruslan Karabalin <karabalin112@gmail.com>
2026-01-31 14:06:49 +0300

    feat: add 0005

--- cpp/s0005.cpp
+++ cpp/s0005.cpp
@@ -4,30 +4,29 @@
 class Solution {
    public:
     std::string longestPalindrome(std::string s) {
-        std::string result = "";
         int len = s.length();
         if (len == 1) {
             return s;
         }
+        int resStart = 0;
+        int resLen = 0;
         for (int i = len; i >= 1; --i) {
             for (int j = 0; j <= len - i; ++j) {
-                std::string subS = s.substr(j, i);
-                if (isPalindrome(subS)) {
-                    result = subS;
+                if (isPalindrome(s, j, j + i - 1)) {
+                    resStart = j;
+                    resLen = i;
                     break;
                 }
             }
-            if (result != "") {
+            if (resLen != 0) {
                 break;
             }
         }
-        return result;
+        return s.substr(resStart, resLen);
     }
 
    private:
-    bool isPalindrome(const std::string& s) {
-        int l = 0;
-        int r = s.length() - 1;
+    bool isPalindrome(const std::string& s, int l, int r) {
         while (l < r) {
             if (s[l] != s[r]) {
                 return false;
